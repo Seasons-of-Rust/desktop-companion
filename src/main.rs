30c53d266692fc32b3109dfae52290a9ea7e4a71
a-llie <alexstefanakis@cmail.carleton.ca>
2022-08-01 12:31:55 -0400

    try bevy implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        // Set antialiasing to use 4 samples
+        .insert_resource(Msaa { samples: 4 })
+        // Set WindowDescriptor Resource to change title and size
+        .insert_resource(WindowDescriptor {
+            title: "Companion!".to_string(),
+            width: 320.0,
+            height: 240.0,
+            transparent: true,
+            decorations: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
