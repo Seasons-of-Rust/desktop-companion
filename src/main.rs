9e5a33061cddab5305b125390bb83148c4f62705
a-llie <alexstefanakis@cmail.carleton.ca>
2022-08-01 19:39:35 -0400

    transparent + always on top :+1:

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
-//! Shows how to display a window in transparent mode.
-//!
-//! This feature works as expected depending on the platform. Please check the
-//! [documentation](https://docs.rs/bevy/latest/bevy/prelude/struct.WindowDescriptor.html#structfield.transparent)
-//! for more details.
-
 use bevy::{prelude::*, window::WindowDescriptor};
+use bevy::window::WindowId;
+use bevy::winit::WinitWindows;
 
 fn main() {
-    App::new()
-        // ClearColor must have 0 alpha, otherwise some color will bleed through
-        .insert_resource(ClearColor(Color::NONE))
-        .insert_resource(WindowDescriptor {
-            // Setting `transparent` allows the `ClearColor`'s alpha value to take effect
-            transparent: true,
-            // Disabling window decorations to make it feel more like a widget than a window
-            decorations: false,
-            ..default()
-        })
-        .add_startup_system(setup)
-        .add_plugins(DefaultPlugins)
-        .run();
+    setup_bevy();
 }
+
+
+fn setup_bevy(){
+    App::new()
+    // ClearColor must have 0 alpha, otherwise some color will bleed through
+    .insert_resource(ClearColor(Color::NONE))
+    .insert_resource(WindowDescriptor {
+        transparent: true,
+        decorations: false,
+        width: 200.0,
+        height: 200.0,
+        resizable: false,
+        ..default()
+    })
+    .add_startup_system(setup)
+    .add_system(setup_winit)
+    .add_plugins(DefaultPlugins)
+    .run();
+}
+
+fn setup_winit(windows: NonSend<WinitWindows>,) {
+    //bevy doesn't have a way to set always_on_top, so use winit 
+    let primary = windows.get_window(WindowId::primary()).unwrap();
+    primary.set_always_on_top(true);
+}
+
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("branding/icon.png"),
+    commands.spawn_bundle(SpriteBundle{
+        texture: asset_server.load("sleepyKitty_Idle00.png"),
         ..default()
     });
 }
